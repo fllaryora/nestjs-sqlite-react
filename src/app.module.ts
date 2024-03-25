a3ac41985b3285518841b9bf4a7b9bd82462e839
Francisco <fllaryora@gmail.com>
2024-03-25 17:11:57 -0300

    simple example

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { GraphQLModule, } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { PetsModule } from './pets/pets.module';
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      //include: [CatsModule],
+      //autoSchemaFile: true,//the schema can be generated on-the-fly in memory
+      //sortSchema: true,
+      //autoSchemaFile: 'schema.gql',//the file is automatically generated
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    PetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
